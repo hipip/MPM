bef2fadf6764245facf7b9a203639c060c1d1fd9
hipip <bt.youcef.dz@gmail.com>
2024-12-05 11:05:31 +0100

    Add basic Tasks Table functionality

---
+++ src/components/TaskTable.js
@@ -0,0 +1,90 @@
+const getTasks = () => {
+  return [...document.querySelectorAll(".task-row")].map((taskRow) => {
+    const [td1, td2, td3] = taskRow.children;
+    const [taskName, taskDuration, taskPredecessors] = [
+      td1.children[0].value,
+      td2.children[0].value,
+      td3.children[0].value,
+    ];
+    return [taskName, taskDuration, taskPredecessors];
+  });
+};
+
+const newRow = () => {
+  const tr = document.createElement("tr");
+  tr.className = "task-row";
+  const taskNameTd = document.createElement("td");
+  const taskDurationTd = document.createElement("td");
+  const taskPredecessorsTd = document.createElement("td");
+  const deleteRowBtn = document.createElement("button");
+  deleteRowBtn.className = "btn delete-row-btn";
+  deleteRowBtn.textContent = "X";
+  deleteRowBtn.onclick = () => {
+    tr.remove();
+  };
+
+  const taskNameInput = document.createElement("input");
+  taskNameInput.className = "inp task-name-inp";
+  const taskDurationInput = document.createElement("input");
+  taskDurationInput.type = "number";
+  taskDurationInput.className = "inp task-duration-inp";
+  const taskPredecessorsInput = document.createElement("input");
+  taskPredecessorsInput.className = "inp task-predecessors-inp";
+
+  taskNameTd.appendChild(taskNameInput);
+  taskDurationTd.appendChild(taskDurationInput);
+  taskPredecessorsTd.appendChild(taskPredecessorsInput);
+
+  tr.appendChild(taskNameTd);
+  tr.appendChild(taskDurationTd);
+  tr.appendChild(taskPredecessorsTd);
+  tr.appendChild(deleteRowBtn);
+
+  return tr;
+};
+
+const TaskTable = () => {
+  const table = document.createElement("table");
+  const thead = document.createElement("thead");
+  const tr = document.createElement("tr");
+  const th1 = document.createElement("th");
+  th1.textContent = "Tâche";
+  const th2 = document.createElement("th");
+  th2.textContent = "Durée";
+  const th3 = document.createElement("th");
+  th3.textContent = "Tâches antérieures";
+
+  const addTaskButton = () => {
+    const cont = document.createElement("tfoot");
+    cont.className = "add-task-row";
+    const tr = document.createElement("tr");
+    const td = document.createElement("td");
+    td.colSpan = "3";
+    const btn = document.createElement("button");
+    btn.type = "button";
+    btn.textContent = "Ajouter Tâche";
+    btn.className = "btn add-task-btn";
+    btn.onclick = () => {
+      table.insertBefore(newRow(), cont);
+      console.table(getTasks());
+    };
+
+    td.appendChild(btn);
+    tr.appendChild(td);
+    cont.appendChild(tr);
+    return cont;
+  };
+
+  tr.appendChild(th1);
+  tr.appendChild(th2);
+  tr.appendChild(th3);
+
+  thead.appendChild(tr);
+
+  table.appendChild(thead);
+  table.appendChild(addTaskButton());
+
+  table.className = "task-table";
+  return table;
+};
+export default TaskTable;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import TaskTable from "./components/TaskTable.js";
+
+const root = document.querySelector("#root");
+
+root.appendChild(TaskTable());
